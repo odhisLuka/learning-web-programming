9e02e4ee648a196f31a9ef005df20bfe5a2b0e1d
odhisLuka <luketrevor03@gmail.com>
2022-10-12 16:53:23 +0300

    added notes on how you can use fstr

--- name.py
+++ name.py
@@ -1,6 +1,10 @@
 from urllib.parse import parse_qs
 
-
 name = input("Name: ")
-print("Hello, " + name)
+# what this code does is it prompts the user to type in their name and save it in a variable calle name
 
+print("Hello, " + name)
+#this code retuns hello, name when the user types in their name
+
+#alternatively, you can use fstr, also called formatted string, to substitute the value of the variable
+print(f"Hello, {name}")
