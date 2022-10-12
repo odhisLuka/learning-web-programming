03713ae80bf19d9ca0653d705c1f87f525ba58ab
odhisLuka <luketrevor03@gmail.com>
2022-10-12 17:36:22 +0300

    added a comment

--- conditions.py
+++ conditions.py
@@ -1,4 +1,6 @@
 n = int(input("Number: "))
+# you need to define the variable n as an integer, otherwise when you run n, you will get a typeError
+# because the computer can't distinguish whether n is a str or and int
 
 if n > 0:
   print("n is positive")
