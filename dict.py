781f91c54bba55bd13df4be401dc668f1b346cc1
odhisLuka <luketrevor03@gmail.com>
2022-10-18 14:01:09 +0300

    added a dictionary file

---
+++ dict.py
@@ -0,0 +1,7 @@
+names = {"Luke":"He is the disciple of JESUS and love HIM so much", "Cecil":"Cecil, also called Zera, is the brother to Luke"}
+
+# adds a new key and value pair
+names["Apollo"] = "He is the twin brother to Luke"
+
+
+print(names["Apollo"])
