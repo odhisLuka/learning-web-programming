eb9bc98ff542e7dc27681cf4cb4963888c80fb79
odhisLuka <luketrevor03@gmail.com>
2022-10-18 13:26:20 +0300

    some files

--- lists.py
+++ lists.py
@@ -1,12 +1,12 @@
 #define a lists of name
 names = ["Harry", "Ron", "Luke", "Odhiambo"]
 
-
+print(names)
 #adds an item  to the list of names
 names.append("Trevor")
 #sort items in list lexographically
 names.sort()
 
-print(names)
+
 
 
