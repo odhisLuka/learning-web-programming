4b5da158487cc7fa751c24ed17d043764aec8a91
odhisLuka <luketrevor03@gmail.com>
2022-10-18 13:10:20 +0300

    adding py files

---
+++ lists.py
@@ -0,0 +1,12 @@
+#define a lists of name
+names = ["Harry", "Ron", "Luke", "Odhiambo"]
+
+
+#adds an item  to the list of names
+names.append("Trevor")
+#sort items in list lexographically
+names.sort()
+
+print(names)
+
+
---
+++ sequences.py
@@ -0,0 +1,24 @@
+#there are several types o sequences, one of them is a string, for intance
+name = "Luke"
+
+#for example, this sequence allows some individual elements inside of a sequence. It uses an array
+print(name[0])
+print(name[1])
+print(name[2])
+# the square bracket takes an ordered sequence of elements inside a sequence and gets me access to that particular elemtent
+# [0] will return letter L
+# [1] will return letter u, etc.
+
+# another sequence you can use is also called lists, as explained below
+
+names = "Luke", "Trevor", "Odhiambo"
+
+print(names[2])
+
+#you can also have the code run all names at once as shown below
+print(names)
+
+#another sequcne is called TUPLES often used if you have a couple values that won't have to change but you need to store a pair of values; immutable values
+
+
+
