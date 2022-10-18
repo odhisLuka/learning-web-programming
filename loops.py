f0511d9067696dfa10c6db65bcdaa964e6df18f8
odhisLuka <luketrevor03@gmail.com>
2022-10-18 13:43:21 +0300

    added some lines

---
+++ loops.py
@@ -0,0 +1,17 @@
+from tkinter.tix import INTEGER
+
+
+#prints interger in range of 6
+i = INTEGER
+for i in range(6):
+  print(i)
+
+# prints all names in alists of names
+names = ["Luke", "Odhiambo", "Trevor"]
+for name in names:
+  print(name)
+
+# prints every character in a name
+name = "Luke"
+for character in name:
+  print(character)
